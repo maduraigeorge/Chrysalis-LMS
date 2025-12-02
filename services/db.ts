d06f40ec713bf089b20ceffbca589ae40f174cfa
maduraigeorge <maduraigeorge@gmail.com>
2025-12-03 01:38:33 +0530

    Update Firebase config to use VITE environment variables

--- services/db.ts
+++ services/db.ts
@@ -7,12 +7,12 @@ const DB_VERSION = 1;
 // --- CONFIGURATION ---
 // This tells the app to look for the keys in Vercel's Environment Variables
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: process.env.VITE_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Cloud DB
